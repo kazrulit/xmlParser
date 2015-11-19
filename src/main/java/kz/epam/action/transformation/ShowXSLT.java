c39b62392d883ae00221673d13706f05b470ce80
admin <mr_spec@mail.ru>
2015-11-19 16:24:47 +0600

    xml beta 3.0

--- src/main/java/kz/epam/action/transformation/ShowXSLT.java
+++ src/main/java/kz/epam/action/transformation/ShowXSLT.java
@@ -9,17 +9,22 @@ import org.apache.struts.action.ActionMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.StringWriter;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by admin on 11/18/2015.
  */
 public class ShowXSLT extends Action {
+    private static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -46,7 +51,10 @@ public class ShowXSLT extends Action {
             transformer.setParameter(Configs.TYPE, request.getParameter(Configs.TYPE));
         }
 
+        readWriteLock.writeLock().lock();
         transformer.transform(xmlSource, streamResult);
+        readWriteLock.writeLock().unlock();
+
         response.getWriter().print(streamResult.getWriter());
 
         return super.execute(mapping, form, request, response);
